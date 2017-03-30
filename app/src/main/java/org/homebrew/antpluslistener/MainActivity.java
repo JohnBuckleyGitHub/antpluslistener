b8a8ceea27eb0f76320556ffef75c81f4792ad97
johnbolia <johnpbcmail@gmail.com>
2017-03-30 18:32:48 +0100

    Power added with menu edit

--- app/src/main/java/org/homebrew/antpluslistener/MainActivity.java
+++ app/src/main/java/org/homebrew/antpluslistener/MainActivity.java
@@ -6,12 +6,18 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    BikeSpeedDistanceSampler instanceObj = new BikeSpeedDistanceSampler(this, this);
-    TextView tv_status;
-    TextView tv_state;
-    TextView tv_code;
-    TextView tv_timeStamp;
-    TextView tv_eventFlags;
+    BikeSpeedDistanceSampler spdCadInstanceObj = new BikeSpeedDistanceSampler(this, this);
+    BikePowerSampler powerInstanceObj = new BikePowerSampler(this, this);
+    TextView tv_spdCadStatus;
+    TextView tv_spdCadState;
+    TextView tv_powerSource;
+    TextView tv_torqueSource;
+    TextView tv_powerStatus;
+    TextView tv_powerState;
+    TextView tv_torque;
+    TextView tv_power;
+    //    TextView tv_timeStamp;
+//    TextView tv_eventFlags;
     TextView tv_timeStampLast;
     TextView tv_cumrev;
     TextView tv_cadTimeStamp;
@@ -22,43 +28,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_status = (TextView)findViewById(R.id.textView_Status);
-        tv_state = (TextView)findViewById(R.id.textView_Status2);
-        tv_code = (TextView)findViewById(R.id.textView_Status3);
-        tv_timeStamp = (TextView)findViewById(R.id.textView_Status4);
-        tv_eventFlags = (TextView)findViewById(R.id.textView_Status5);
-        tv_timeStampLast = (TextView)findViewById(R.id.textView_Status6);
-        tv_cumrev = (TextView)findViewById(R.id.textView_Status7);
-        tv_cadTimeStamp = (TextView)findViewById(R.id.textView_Status8);
-        tv_cadTimeStampLast = (TextView)findViewById(R.id.textView_Status9);
+        tv_spdCadStatus = (TextView)findViewById(R.id.textView_Status);
+        tv_spdCadState = (TextView)findViewById(R.id.textView_Status2);
+        tv_powerSource = (TextView)findViewById(R.id.textView_Status3);
+        tv_powerStatus = (TextView)findViewById(R.id.textView_Status4);
+        tv_powerState = (TextView)findViewById(R.id.textView_Status5);
+        tv_power = (TextView)findViewById(R.id.textView_Status6);
+//        tv_timeStampLast = (TextView)findViewById(R.id.textView_Status6);
+        tv_torque = (TextView)findViewById(R.id.textView_Status7);
+//        tv_cumrev = (TextView)findViewById(R.id.textView_Status7);
+//        tv_cadTimeStamp = (TextView)findViewById(R.id.textView_Status8);
+        tv_torqueSource = (TextView)findViewById(R.id.textView_Status9);
         tv_cadCumrev = (TextView)findViewById(R.id.textView_Status10);
-
-        tv_status.setText("Name here");
+//        tv_cadCumrev = (TextView)findViewById(R.id.textView_Status10);
+        tv_spdCadStatus.setText("Name here");
     }
 
     /** Called when the user clicks the connect button */
     public void activateSearch(View view) {
-        instanceObj.resetPcc();
-
+        spdCadInstanceObj.resetPcc();
     }
-
+    /** Called when the user clicks the connect button */
+    public void activatePowerSearch(View view) {
+        powerInstanceObj.resetPcc();
+    }
     public void closeThreads(View view) {
-        instanceObj.closePcc();
-
+        spdCadInstanceObj.closePcc();
     }
 
     /** Called when the user clicks the Send button */
     public void momentaryListen(View view) {
-        tv_status.setText(instanceObj.resultsMap.get("spdDeviceName"));
-        tv_state.setText(instanceObj.resultsMap.get("spdDeviceState"));
-        tv_code.setText(instanceObj.resultsMap.get("spdResultCode"));
-        tv_timeStamp.setText(String.valueOf(instanceObj.spdEstTimestamp));
-        tv_eventFlags.setText(String.valueOf(instanceObj.spdEventFlags));
-        tv_timeStampLast.setText(String.valueOf(instanceObj.spdTimestampOfLastEvent));
-        tv_cumrev.setText(String.valueOf(instanceObj.spdCumulativeRevolutions));
-        tv_cadTimeStamp.setText(String.valueOf(instanceObj.cadEstTimestamp));
-        tv_cadTimeStampLast.setText(String.valueOf(instanceObj.cadTimestampOfLastEvent));
-        tv_cadCumrev.setText(String.valueOf(instanceObj.cadCumulativeRevolutions));
+        tv_spdCadStatus.setText(spdCadInstanceObj.resultsMap.get("spdDeviceName"));
+        tv_spdCadState.setText(spdCadInstanceObj.resultsMap.get("spdDeviceState"));
+        tv_powerSource.setText(powerInstanceObj.resultsMap.get("calculatedPowerSource"));
+        tv_powerStatus.setText(powerInstanceObj.resultsMap.get("powerDeviceName"));
+        tv_powerState.setText(powerInstanceObj.resultsMap.get("powerDeviceState"));
+        tv_torque.setText(String.valueOf(powerInstanceObj.calculatedTorque));
+        tv_power.setText(String.valueOf(powerInstanceObj.calculatedPower));
+//        tv_cumrev.setText(String.valueOf(spdCadInstanceObj.spdCumulativeRevolutions));
+//        tv_cadTimeStamp.setText(String.valueOf(spdCadInstanceObj.cadEstTimestamp));
+        tv_torqueSource.setText(String.valueOf(spdCadInstanceObj.cadTimestampOfLastEvent));
+        tv_cadCumrev.setText(powerInstanceObj.resultsMap.get("calculatedTorqueSource"));
 
     }
 
